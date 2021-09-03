ab51d51d3d79ef7d7e123bda4d4fe42975850405
sudhaknv <sudhaknv@gmail.com>
2021-09-03 23:51:27 +0530

    casino code

---
+++ src/test/java/Bonus/AppTest.java
@@ -0,0 +1,20 @@
+package Bonus;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
